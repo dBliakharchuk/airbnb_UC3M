36a8ae80e2878b5e46e331eccf358472ee87acc4
Mateusz <nologin90@gmail.com>
2018-10-04 22:52:11 +0200

    servlet skeleton

---
+++ airbnb/src/main/java/org/uc3m/ctftw/airbnb/servlet/TripServlet.java
@@ -0,0 +1,55 @@
+package org.uc3m.ctftw.airbnb.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet(
+		urlPatterns="/login",
+		loadOnStartup=1,
+		initParams={@WebInitParam(name="configuracion", value="es.uc3m.tiw")}
+		)
+public class TripServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private ServletConfig config;
+	
+		@Override
+		public void init(ServletConfig config) throws ServletException {
+			this.config = config;
+			
+		}
+	       
+
+
+		/**
+		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			config.getServletContext().getRequestDispatcher(LOGIN_JSP).forward(request, response);
+		}
+
+		/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+			
+		}
+
+
+}
