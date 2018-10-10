dfa649b67b26fdc0052f0e8f4278a78c82f37f1b
Piotr Ducki <duckipiotr@gmail.com>
2018-10-10 20:47:55 +0200

    classes prototypes added

--- airbnb/src/main/java/org/uc3m/ctftw/airbnb/database/DataAccess.java
+++ airbnb/src/main/java/org/uc3m/ctftw/airbnb/database/DataAccess.java
@@ -7,35 +7,45 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DataAccess {
+public class DataAccess
+{
 	private static Connection connection;
-	
-	public static void initialize() {
-		try {
+
+	public static void initialize()
+	{
+		try
+		{
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sampledb?autoReconnect=true&useSSL=false", 
-					"userLQE", "2dAlhk2RqPhVlFOK");
-			Statement stmt = connection.createStatement(); 
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://127.0.0.1:3306/sampledb?autoReconnect=true&useSSL=false", "userLQE",
+					"2dAlhk2RqPhVlFOK");
+			Statement stmt = connection.createStatement();
 			ResultSet rs = stmt.executeQuery("select * from users;");
-			
+
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int columnsNumber = rsmd.getColumnCount();
-			while (rs.next()) {
-			    for (int i = 1; i <= columnsNumber; i++) {
-			        if (i > 1) System.out.print(",  ");
-			        String columnValue = rs.getString(i);
-			        System.out.print(columnValue + " " + rsmd.getColumnName(i));
-			    }
-			    System.out.println("");
+			while (rs.next())
+			{
+				for (int i = 1; i <= columnsNumber; i++)
+				{
+					if (i > 1)
+						System.out.print(",  ");
+					String columnValue = rs.getString(i);
+					System.out.print(columnValue + " " + rsmd.getColumnName(i));
+				}
+				System.out.println("");
 			}
-			
-			if (rs != null) {
+
+			if (rs != null)
+			{
 				rs.close();
 			}
-			if (stmt != null) {
+			if (stmt != null)
+			{
 				stmt.close();
 			}
-		} catch (Exception ex) {
+		} catch (Exception ex)
+		{
 			ex.printStackTrace();
 		}
 	}
