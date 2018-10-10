01363b23d8feaa26d495383bd28492aaa6f75ce2
Piotr Ducki <duckipiotr@gmail.com>
2018-10-10 20:53:55 +0200

    classes prototypes added

--- airbnb/src/main/java/model/Localization.java
+++ airbnb/src/main/java/model/Localization.java
@@ -2,5 +2,11 @@ package model;
 
 public class Localization
 {
-
+	private String country;
+	private String city;
+	private String street;
+	private int buildingNumber;
+	private int flatNumber;
+	
+	
 }
