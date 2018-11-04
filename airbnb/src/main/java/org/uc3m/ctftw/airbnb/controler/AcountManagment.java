529912d3826edc2d18d81667fed16fa37004e40a
Piotr Ducki <duckipiotr@gmail.com>
2018-11-04 12:02:31 +0100

    administrato views added(log in, users)

---
+++ airbnb/src/main/java/org/uc3m/ctftw/airbnb/controler/AcountManagment.java
@@ -0,0 +1,18 @@
+package org.uc3m.ctfw.airbnb.controler;
+
+import org.uc3m.ctftw.airbnb.model.User;
+
+public class AcountManagment
+{
+	public void registerUser(User user)
+	{
+		
+	}
+	
+	public void logInUser(String email, String password)
+	{
+		
+	}
+	
+	
+}
